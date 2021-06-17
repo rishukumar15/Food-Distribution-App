4b390e9efaf742fa06a1b3c849954c174ff18d29
rishukumar15 <57403537+rishukumar15@users.noreply.github.com>
2021-06-17 21:20:47 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rishu5727@gmail.com"
-EMAIL_HOST_PASSWORD = "Rishu123@rock08"
+EMAIL_HOST_USER = "example@gmail.com"
+EMAIL_HOST_PASSWORD = "######"
